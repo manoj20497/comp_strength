3495de3d315e685d7f3dc9308a6632f184abb54e
manoj20497 <136904585+manoj20497@users.noreply.github.com>
2023-06-18 00:16:58 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pickle as pkl
+from PIL import Image
 
 loaded_model = pkl.load(open("trained_model.sav", "rb"))
 
@@ -23,27 +24,42 @@ def strength_prediction(input_data):
 
 
 def main():
-    
-    # Giving a title
     st.title("Compressive Strength Web App")
     
+    pic = Image.open("con.jpg")
+    st.image(pic, caption="Just an Image", width= 400,channels="RGB")
+    # Giving a title
+    
+    st.write("""
+    ## Calculate the Compressive Strength
+    ### by Giving mix design as an input
+    """)
+    
     # Getting the input data from the user
-   
-    cement = st.text_input("Cement Content")
+    age = st.sidebar.selectbox("Testing period (in days)", ("3","7", "14", "28"))
+    cement = st.sidebar.slider("Cement Content (in Kg)", 300, 550)
+    # cement = st.text_input("Cement Content")
     blastFurnace = st.text_input("BlastFurnance Content")
     flyAsh = st.text_input("FlyAsh Content")
     Water = st.text_input("Water Content")
     Plasticizer = st.text_input("Plasticizer Content")
     CourseAgg = st.text_input("CourseAgg Content")
     FineAgg = st.text_input("FineAgg Content")
-    Age = st.text_input("Testing period")
+    # if age == "3":
+    #     Age = 3
+    # elif age == "7":
+    #     Age = 3
+    # elif age == "14":
+    #     Age = 14
+    # else: 
+    #     Age = 28
     
     # Code for prediction
     result = ""
     
     #Creating a button for prediction
     if st.button("Calculate"):
-        result = strength_prediction([cement,blastFurnace,flyAsh,Water,Plasticizer,CourseAgg,FineAgg,Age])
+        result = strength_prediction([cement,blastFurnace,flyAsh,Water,Plasticizer,CourseAgg,FineAgg,age])
      
     st.success(result)
     
